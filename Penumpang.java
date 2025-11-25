a128136eac044637b3ff54eb107cbbabffd3fdca
KhalishaUfairah <khalishaufairah@gmail.com>
2025-11-25 20:20:18 +0700

    Penumpang.java

--- Penumpang.java
+++ Penumpang.java
@@ -12,3 +12,30 @@ public class Penumpang implements PembayaranOngkos {
         this.hamil = hamil;
         this.saldo = 10000;
     }
+
+public int getID() { return id; }
+    public String getNama() { return nama; }
+    public int getUmur() { return umur; }
+    public boolean getHamil() { return hamil; }
+    public int getSaldo() { return saldo; }
+
+    public void tambahSaldo(int s) {
+        saldo += s;
+    }
+
+    @Override
+    public void bayarOngkos(int ongkos) throws SaldoTidakCukupException {
+        if (saldo < ongkos)
+            throw new SaldoTidakCukupException("Saldo tidak cukup untuk " + nama);
+        saldo -= ongkos;
+    }
+
+    public boolean isPrioritas() {
+        return umur > 60 || umur < 10 || hamil;
+    }
+
+    public String toString() {
+        return "[" + id + "] " + nama + " | Umur: " + umur + 
+               " | Hamil: " + hamil + " | Saldo: " + saldo;
+    }
+} 
