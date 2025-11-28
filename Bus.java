97ba1d56ca5208fb51b4e94d7b2c39059de97a36
KhalishaUfairah <khalishaufairah@gmail.com>
2025-11-28 19:31:19 +0700

    Bus.java

--- Bus.java
+++ Bus.java
@@ -6,4 +6,11 @@ public class Bus {
     private ArrayList<Penumpang> penumpangBiasa;
     private ArrayList<Penumpang> penumpangBerdiri;
 
-  
+    public static final int ONGKOS_BUS = 2000;
+    private int totalPendapatan = 0;
+
+    public Bus() {
+        penumpangPrioritas = new ArrayList<>();
+        penumpangBiasa = new ArrayList<>();
+        penumpangBerdiri = new ArrayList<>();
+    }
