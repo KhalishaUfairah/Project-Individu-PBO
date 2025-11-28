0fe910c2cdfd1262222702c8ee2a3e0424fe7192
KhalishaUfairah <khalishaufairah@gmail.com>
2025-11-28 19:30:52 +0700

    Bus.java

--- Bus.java
+++ Bus.java
@@ -1,1 +1,9 @@
+import java.util.ArrayList;
 
+public class Bus {
+
+    private ArrayList<Penumpang> penumpangPrioritas;
+    private ArrayList<Penumpang> penumpangBiasa;
+    private ArrayList<Penumpang> penumpangBerdiri;
+
+  
