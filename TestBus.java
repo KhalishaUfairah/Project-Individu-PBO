3991d55401f43a67ebe15282bfcb094a39b2abd2
KhalishaUfairah <khalishaufairah@gmail.com>
2025-11-30 19:40:36 +0700

    TestBus.java

--- TestBus.java
+++ TestBus.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class TestBus {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Bus bus = new Bus();
+
+        while (true) {
+            System.out.println("===== BUS TRANS KOETARADJA =====");
+            System.out.println("1. Naikkan Penumpang");
+            System.out.println("2. Turunkan Penumpang");
+            System.out.println("3. Lihat Penumpang");
+            System.out.println("4. Keluar");
+            System.out.print("Pilihan: ");
+            int pilihan = in.nextInt();
